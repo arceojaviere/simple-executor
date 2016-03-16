b8b7a8cf2cacaed884125ccd1956e326271ba400
user1 <mail@email.com>
2016-03-15 18:04:46 -0600

    Aesthetic changes. Changed maven group ID, package and PID names

--- src/main/java/com/rsa/proc/jmx/ConfigReloaderMBean.java
+++
@@ -1,7 +0,0 @@
-package com.rsa.proc.jmx;
-
-import com.rsa.proc.config.ConfigurationLoadingException;
-
-public interface ConfigReloaderMBean {
-	public abstract void reloadConfiguration() throws ConfigurationLoadingException;
-}
